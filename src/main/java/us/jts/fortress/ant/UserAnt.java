f2a00a1e03d72616db2c7ab999f4d781962ec7da
Shawn McKinney <shawn.mckinney@jts.us>
2013-09-18 22:16:28 -0500

    FC-67 - Add User properties to Ant admin

--- src/main/java/us/jts/fortress/ant/UserAnt.java
+++ src/main/java/us/jts/fortress/ant/UserAnt.java
@@ -7,6 +7,7 @@ package us.jts.fortress.ant;
 
 import us.jts.fortress.rbac.User;
 import us.jts.fortress.rbac.UserRole;
+import us.jts.fortress.util.attr.AttrHelper;
 
 import java.util.StringTokenizer;
 
@@ -18,7 +19,7 @@ import java.util.StringTokenizer;
  */
 public class UserAnt extends User
 {
-    private String antRoles;
+    private String userProps;
     private String email;
     private String phone;
     private String mobile;
@@ -49,6 +50,17 @@ public class UserAnt extends User
                  getAddress().setAddress(aTkn);
              }
          }
+    }
+
+    public String getUserProps()
+    {
+        return userProps;
+    }
+
+    public void setUserProps( String userProps )
+    {
+        this.userProps = userProps;
+        addProperties(AttrHelper.getProperties(userProps));
     }
 
     public String getPostalCode()
@@ -135,16 +147,6 @@ public class UserAnt extends User
 
 
     /**
-     * Return a comma delimited string that contains the User's roles.
-     *
-     * @return variable contains a comma delimited collection of role names.
-     */
-    public String getAntRoles()
-    {
-        return antRoles;
-    }
-
-    /**
      * Generally not good practice to handle passwords as Strings in Java but this method allows Ant's digester to consume field in String format from the xml input file.
      * It subsequently converts to char[] as needed by the parent entity - {@link User}.
      *
@@ -153,28 +155,6 @@ public class UserAnt extends User
     public void setPassword(String password)
     {
         super.setPassword(password.toCharArray());
-    }
-
-    /**
-     * Accept a comma delimited String containing a list of Roles to be granted to a user.  This function
-     * will parse the String and call the setter on its parent.
-     *
-     * @param antRoles contains a comma delimited set of role names.
-     */
-    public void setAntRoles(String antRoles)
-    {
-        this.antRoles = antRoles;
-        // allow the setter to process comma delimited strings:
-        StringTokenizer tkn = new StringTokenizer(antRoles, ",");
-        if (tkn.countTokens() > 0)
-        {
-            while (tkn.hasMoreTokens())
-            {
-                String rTkn = tkn.nextToken();
-                UserRole ur = new UserRole(rTkn);
-                setRole(ur);
-            }
-        }
     }
 
     public String getPhone()
