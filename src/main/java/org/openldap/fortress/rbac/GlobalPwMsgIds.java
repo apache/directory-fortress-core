e5ec67a02dc867cd28619dae6cbf695751869c59
Emmanuel Lécharny <elecharny@symas.com>
2014-10-21 20:14:23 +0200

    Added teh apache license header in all the files

--- src/main/java/org/openldap/fortress/rbac/GlobalPwMsgIds.java
+++ src/main/java/org/openldap/fortress/rbac/GlobalPwMsgIds.java
@@ -1,18 +1,22 @@
 /*
- * This work is part of OpenLDAP Software <http://www.openldap.org/>.
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
  *
- * Copyright 1998-2014 The OpenLDAP Foundation.
- * All rights reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted only as authorized by the OpenLDAP
- * Public License.
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
  *
- * A copy of this license is available in the file LICENSE in the
- * top-level directory of the distribution or, alternatively, at
- * <http://www.OpenLDAP.org/license.html>.
  */
-
 package org.openldap.fortress.rbac;
 
 /**
