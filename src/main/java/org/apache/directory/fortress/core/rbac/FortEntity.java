c1d87e8d9e395189747f4b5c2e12808ac074bf33
Emmanuel Lécharny <elecharny@symas.com>
2014-11-02 16:59:58 +0100

    Reformatted the code

--- src/main/java/org/apache/directory/fortress/core/rbac/FortEntity.java
+++ src/main/java/org/apache/directory/fortress/core/rbac/FortEntity.java
@@ -90,147 +90,147 @@ import javax.xml.bind.annotation.XmlType;
 })
 public abstract class FortEntity
 {
-protected String modCode;
-protected String modId;
-@XmlTransient
-protected Session adminSession;
-protected long sequenceId;
-@XmlTransient
-protected String contextId;
-
-
-/**
- * Default constructor will call the setter to load a new internal ID into entity.
- */
-public FortEntity()
-{
-    setInternalId();
-}
-
-
-/**
- * Use this constructor to load administrative RBAC session into this entity.
- *
- * @param adminSession contains ARBAC Session object.
- */
-public FortEntity( Session adminSession )
-{
-    setInternalId();
-    this.adminSession = adminSession;
-}
-
-
-/**
- * This attribute is required but is set automatically by Fortress DAO class before object is persisted to ldap.
- * This generated internal id is associated with PermObj.  This method is used by DAO class and
- * is not available to outside classes.   The generated attribute maps to 'ftId' in 'ftObject' object class.
- */
-private void setInternalId()
-{
-    UUID uuid = UUID.randomUUID();
-    this.modId = uuid.toString();
-}
-
-
-/**
- * Return the ARBAC Session object that was loaded into this entity.
- *
- * @return ARBAC Session object.
- */
-public Session getAdminSession()
-{
-    return adminSession;
-}
-
-
-/**
- * Load an ARBAC Session object into this entity.  Once loaded, all Fortress Manager's will perform administrative
- * permission checks against the User who is contained within the Session.
- *
- * @param adminSession
- */
-public void setAdminSession( Session adminSession )
-{
-    this.adminSession = adminSession;
-}
-
-
-/**
- * Contains the Fortress modification code to be associated with an audit record.  This is the ObjectName.methodName
- * for the Manager API that was called.
- *
- * @return String contains the modification code maps to 'ftModCode' attribute in 'FortEntity' object class.
- */
-public String getModCode()
-{
-    return modCode;
-}
-
-
-/**
- * Set the Fortress modification code to be associated with an audit record.  Contains the Fortress modification code
- * which is ObjectName.methodName for the Manager API that was called.
- *
- * @param modCode contains the modification code maps to 'ftModCode' attribute in 'FortEntity' object class.
- */
-public void setModCode( String modCode )
-{
-    this.modCode = modCode;
-}
-
-
-/**
- * Get the unique ID that is to be associated with a particular audit record in directory.
- *
- * @return attribute that maps to 'ftModId' attribute in 'FortEntity' object class.
- */
-public String getModId()
-{
-    return modId;
-}
-
-
-/**
- * Return the contextId for this record.  The contextId is used for multi-tenancy to isolate data sets within a particular sub-tree within DIT
- *
- * @return value maps to sub-tree in DIT, for example ou=contextId, dc=jts, dc = com.
- */
-public String getContextId()
-{
-    return contextId;
-}
-
-
-/**
- * Set the contextId associated with this record.  The contextId is used for multi-tenancy to isolate data sets within a particular sub-tree within DIT.
- * Package private to prevent outside classes from setting.
- *
- * @param contextId maps to sub-tree in DIT, for example ou=contextId, dc=jts, dc = com.
- */
-public void setContextId( String contextId )
-{
-    this.contextId = contextId;
-}
-
-
-/**
- * Sequence id is used internal to Fortress.
- *
- * @return long value contains sequence id.
- */
-public long getSequenceId()
-{
-    return sequenceId;
-}
-
-
-/**
- * Sequence id is used internal to Fortress
- *
- * @param sequenceId contains sequence to use.
- */
-public void setSequenceId( long sequenceId )
-{
-    this.sequenceId = sequenceId;
-}
+    protected String modCode;
+    protected String modId;
+    @XmlTransient
+    protected Session adminSession;
+    protected long sequenceId;
+    @XmlTransient
+    protected String contextId;
+    
+    
+    /**
+     * Default constructor will call the setter to load a new internal ID into entity.
+     */
+    public FortEntity()
+    {
+        setInternalId();
+    }
+    
+    
+    /**
+     * Use this constructor to load administrative RBAC session into this entity.
+     *
+     * @param adminSession contains ARBAC Session object.
+     */
+    public FortEntity( Session adminSession )
+    {
+        setInternalId();
+        this.adminSession = adminSession;
+    }
+    
+    
+    /**
+     * This attribute is required but is set automatically by Fortress DAO class before object is persisted to ldap.
+     * This generated internal id is associated with PermObj.  This method is used by DAO class and
+     * is not available to outside classes.   The generated attribute maps to 'ftId' in 'ftObject' object class.
+     */
+    private void setInternalId()
+    {
+        UUID uuid = UUID.randomUUID();
+        this.modId = uuid.toString();
+    }
+    
+    
+    /**
+     * Return the ARBAC Session object that was loaded into this entity.
+     *
+     * @return ARBAC Session object.
+     */
+    public Session getAdminSession()
+    {
+        return adminSession;
+    }
+    
+    
+    /**
+     * Load an ARBAC Session object into this entity.  Once loaded, all Fortress Manager's will perform administrative
+     * permission checks against the User who is contained within the Session.
+     *
+     * @param adminSession
+     */
+    public void setAdminSession( Session adminSession )
+    {
+        this.adminSession = adminSession;
+    }
+    
+    
+    /**
+     * Contains the Fortress modification code to be associated with an audit record.  This is the ObjectName.methodName
+     * for the Manager API that was called.
+     *
+     * @return String contains the modification code maps to 'ftModCode' attribute in 'FortEntity' object class.
+     */
+    public String getModCode()
+    {
+        return modCode;
+    }
+    
+    
+    /**
+     * Set the Fortress modification code to be associated with an audit record.  Contains the Fortress modification code
+     * which is ObjectName.methodName for the Manager API that was called.
+     *
+     * @param modCode contains the modification code maps to 'ftModCode' attribute in 'FortEntity' object class.
+     */
+    public void setModCode( String modCode )
+    {
+        this.modCode = modCode;
+    }
+    
+    
+    /**
+     * Get the unique ID that is to be associated with a particular audit record in directory.
+     *
+     * @return attribute that maps to 'ftModId' attribute in 'FortEntity' object class.
+     */
+    public String getModId()
+    {
+        return modId;
+    }
+    
+    
+    /**
+     * Return the contextId for this record.  The contextId is used for multi-tenancy to isolate data sets within a particular sub-tree within DIT
+     *
+     * @return value maps to sub-tree in DIT, for example ou=contextId, dc=jts, dc = com.
+     */
+    public String getContextId()
+    {
+        return contextId;
+    }
+    
+    
+    /**
+     * Set the contextId associated with this record.  The contextId is used for multi-tenancy to isolate data sets within a particular sub-tree within DIT.
+     * Package private to prevent outside classes from setting.
+     *
+     * @param contextId maps to sub-tree in DIT, for example ou=contextId, dc=jts, dc = com.
+     */
+    public void setContextId( String contextId )
+    {
+        this.contextId = contextId;
+    }
+    
+    
+    /**
+     * Sequence id is used internal to Fortress.
+     *
+     * @return long value contains sequence id.
+     */
+    public long getSequenceId()
+    {
+        return sequenceId;
+    }
+    
+    
+    /**
+     * Sequence id is used internal to Fortress
+     *
+     * @param sequenceId contains sequence to use.
+     */
+    public void setSequenceId( long sequenceId )
+    {
+        this.sequenceId = sequenceId;
+    }
 }
