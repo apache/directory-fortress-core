804ca390fb55146696703441741a2ee32843edbf
Shawn McKinney <smckinney@apache.org>
2014-11-03 17:02:53 -0600

    FC-38 - Potential issues on synchronized protected elements - modified getGraph code to workaround deadlock.

--- src/main/java/org/apache/directory/fortress/core/rbac/PsoUtil.java
+++ src/main/java/org/apache/directory/fortress/core/rbac/PsoUtil.java
@@ -267,12 +267,13 @@ public final class PsoUtil
                     hierLock.readLock().unlock();
                     hierLock.writeLock().lock();
 
-                    graph = ( SimpleDirectedGraph<String, Relationship> ) psoCache.get( key );
+                    // TODO: determine why this (code that was commented out) creates a deadlock:
+                    //graph = ( SimpleDirectedGraph<String, Relationship> ) psoCache.get( key );
 
-                    if ( graph == null )
-                    {
+                    //if ( graph == null )
+                    //{
                         graph = loadGraph( contextId );
-                    }
+                    //}
 
                     hierLock.readLock().lock();
                 }
