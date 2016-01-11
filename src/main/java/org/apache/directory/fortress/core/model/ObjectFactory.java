cdb2e2fb78b2743662d96699748ed2f80d73b142
Emmanuel Lécharny <elecharny@symas.com>
2016-01-11 10:01:29 +0100

    Improved Javadoc

--- src/main/java/org/apache/directory/fortress/core/model/ObjectFactory.java
+++ src/main/java/org/apache/directory/fortress/core/model/ObjectFactory.java
@@ -27,19 +27,15 @@ import javax.xml.namespace.QName;
 
 
 /**
- *
- * This class contains factory methods for each Java content interface and Java element interface
- * generated in the org.apache.directory.fortress packages.
- * <p>An ObjectFactory allows you to programatically
- * construct new instances of the Java representation
- * for XML content. The Java representation of XML
- * content can consist of schema derived interfaces
- * and classes representing the binding of schema
- * type definitions, element declarations and model
- * groups.  Factory methods for each of these are
- * provided in this class.
+ * This class contains factory methods for each Java content interface and Java element interface generated in the 
+ * org.apache.directory.fortress packages.
+ * <p>
+ * An ObjectFactory allows you to programatically construct new instances of the Java representation for XML content. 
+ * The Java representation of XML content can consist of schema derived interfaces and classes representing the binding of 
+ * schema type definitions, element declarations and model groups.  Factory methods for each of these are provided in this
+ * class.
+ * 
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
- *
  */
 @XmlRegistry
 public class ObjectFactory
