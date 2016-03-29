12dd5608df3cd21ae61db37efd29ddd546d6242b
Shawn McKinney <smckinney@apache.org>
2016-03-28 23:51:29 -0500

    stop carrying the  sudo pw in the slapd setup

--- ldap/setup/startSlapd.sh
+++ ldap/setup/startSlapd.sh
@@ -20,18 +20,4 @@
 #Echo on
 
 export SLAPD="$1"
-
-if [ -z $2 ]
-then
-    echo 'sudo NOT enabled'	
-else
-    export SUDO=true
-    export SUPW=$2
-    fi
-
-if [ $SUDO ]
-then
-    echo $SUPW | sudo -S $SLAPD
-else
-    $SLAPD
-    fi
+$SLAPD
