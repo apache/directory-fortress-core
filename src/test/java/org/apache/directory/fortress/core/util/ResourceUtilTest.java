40eb8c309250acd5265590066dbd61c8a79ff321
Shawn McKinney <smckinney@symas.com>
2023-06-26 14:47:24 -0500

    format

--- src/test/java/org/apache/directory/fortress/core/util/ResourceUtilTest.java
+++ src/test/java/org/apache/directory/fortress/core/util/ResourceUtilTest.java
@@ -24,19 +24,19 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 
 import static org.junit.Assert.*;
 
 /**
- *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  */
-public class ResourceUtilTest {
+public class ResourceUtilTest
+{
 
 
     @Test
-    public void test_File_Does_Not_Exist_returns_null() {
+    public void test_File_Does_Not_Exist_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream("mumble jumble");
         assertNull("Should be null", inputStream);
     }
@@ -46,13 +46,15 @@ public class ResourceUtilTest {
      * This is a "special" variant of the test_resource_dir_returns_null().
      */
     @Test
-    public void test_empty_string_returns_null() {
+    public void test_empty_string_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream("");
         assertNull("Should be null", inputStream);
     }
 
     @Test
-    public void test_resource_dir_returns_null() {
+    public void test_resource_dir_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream("org");
         assertNull("Should be null", inputStream);
     }
@@ -61,7 +63,8 @@ public class ResourceUtilTest {
      * Translates into a directory and the and the base root of resources
      */
     @Test
-    public void test_dot_string_returns_null() {
+    public void test_dot_string_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream(".");
         assertNull("Should be null", inputStream);
     }
@@ -70,19 +73,22 @@ public class ResourceUtilTest {
      * Translates into a directory and the and the base root of resources
      */
     @Test
-    public void test_slash_string_returns_null() {
+    public void test_slash_string_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream("/");
         assertNull("Should be null", inputStream);
     }
 
     @Test
-    public void test_null_string_returns_null() {
+    public void test_null_string_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream(null);
         assertNull("Should be null", inputStream);
     }
 
     @Test
-    public void test_file_found_in_relative_path_returns_null() {
+    public void test_file_found_in_relative_path_returns_null()
+    {
         File file = new File("config/ehcache.xml");
         assertTrue("File in relative path should exist, just for test, any file will do", file.exists() && !file.isAbsolute() && !file.isDirectory());
         InputStream inputStream = ResourceUtil.getInputStream("config/ehcache.xml");
@@ -90,7 +96,8 @@ public class ResourceUtilTest {
     }
 
     @Test
-    public void test_dir_returns_null() {
+    public void test_dir_returns_null()
+    {
         String tmpDir = System.getProperty("java.io.tmpdir");
         assertNotNull("Didn't find a temp dir", tmpDir);
         assertFalse("Didn't find a temp dir", tmpDir.isEmpty());
@@ -100,13 +107,15 @@ public class ResourceUtilTest {
     }
 
     @Test
-    public void test_web_addr_returns_null() {
+    public void test_web_addr_returns_null()
+    {
         InputStream inputStream = ResourceUtil.getInputStream("http://google.com");
         assertNull("Should be null", inputStream);
     }
 
     @Test
-    public void test_resource_file_returns_input_stream() throws IOException {
+    public void test_resource_file_returns_input_stream() throws IOException
+    {
         String reference = "fortress.properties";
         File file = new File(reference);
         assertTrue("File should not exist in relative path.", !file.exists());
@@ -115,7 +124,8 @@ public class ResourceUtilTest {
     }
 
     @Test
-    public void test_resource_file_in_folder_returns_input_stream() throws IOException {
+    public void test_resource_file_in_folder_returns_input_stream() throws IOException
+    {
         String reference = "bootstrap/ehcache.xml";
         File file = new File(reference);
         assertTrue("File should not exist in relative path.", !file.exists());
