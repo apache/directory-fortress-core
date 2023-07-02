18811d620a0b6f78cb211afe9779cabadab75ab4
Shawn McKinney <smckinney@symas.com>
2023-07-02 18:01:49 -0500

    the userId gen fixed

--- src/test/java/org/apache/directory/fortress/core/jmeter/CheckUser.java
+++ src/test/java/org/apache/directory/fortress/core/jmeter/CheckUser.java
@@ -43,7 +43,7 @@ public class CheckUser extends UserBase
      */
     public SampleResult runTest( JavaSamplerContext samplerContext )
     {
-        String userId  = hostname + '-' + qualifier + '-' + getKey();
+        String userId  = hostname + '-' + qualifier + '-' + getRandomNumber();
         SampleResult sampleResult = new SampleResult();
         try
         {
